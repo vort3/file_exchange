fe303844580909475c846ce0effce53e5bec72ba
Gulam Sheri <sherigulam@gmail.com>
2015-06-27 23:02:09 +0600

    deleted some trash to add more trash

--- receiver.py
+++ receiver.py
@@ -1,9 +1,9 @@
 import socket
-import os
 
 
 KB = 1024
 MB = 1024 * KB
+filebuffer = 4 * KB
 
 def push_and_pull(msg):
     msg = str(msg)
@@ -27,6 +27,11 @@ def pull_and_push(e=None):
     sock.send(input*4)
     return input
 
+def getsize(path):
+    with file(path) as f:
+        f.seek(0, 2)
+        return f.tell()
+
 
 listener = socket.socket()
 listener.bind(('', 25565))
@@ -37,29 +42,25 @@ print "accepted", addr
 expected = int(pull_and_push())
 print "Expected size is", expected
 
-with open("D:\\test.jpg", 'wb+') as f:
+filename = "D:\\test.jpg"
+with open(filename, 'wb+') as f:
     s = 0
     c = 0
     trash = None
-    while s <= expected+512*KB:
+    while s != expected:
         c += 1
-        obj = sock.recv(4*MB)
+        obj = sock.recv(filebuffer)
         if not obj:
             continue
-        if "END_OF_FILE" in obj:
-            obj, trash = obj.split('END_OF_FILE', 1)
         f.write(obj)
-        s += obj.__sizeof__()
+        s += len(obj)
         percents = (s*100)/(expected)
         if c % 100 == 0:
             print "Current:", s, "\t\tExpected:", 
             print expected, '\t', percents, '%'
-        if trash:
-            break
 
 
-s = os.path.getsize("D:\\test.jpg")
-push_and_pull(s)
+push_and_pull(getsize(filename))
 print "File recieved and file size checked"
 
 sock.close()
